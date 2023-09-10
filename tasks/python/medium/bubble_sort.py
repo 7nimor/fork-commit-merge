c62c70944c38e9e0be25861b415b2747a8954d36
Nima Orangi <123732222+7nimor@users.noreply.github.com>
2023-09-10 18:32:37 +0330

    Update bubble_sort.py

--- tasks/python/medium/bubble_sort.py
+++ tasks/python/medium/bubble_sort.py
@@ -1,9 +1,11 @@
 # Python - Medium
 
 def bubble_sort(arr):
-    # TODO: Implement the bubble sort algorithm
+    for l in range(0, len(arr)):
+        for m in range(l+ 1, len(arr)):
+            if arr[l] >= arr[m]:
+                arr[l], arr[m] = arr[m], arr[l]
 
-#! Test cases (Don't edit):
 arr = [64, 25, 12, 22, 11]
 print("Original array:", arr)
 
